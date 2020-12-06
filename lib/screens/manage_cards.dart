b127fd3fe5de9e02e0d05f98d48ed1839cdf4b1e
vikramvaishnav <vikramvaishnav.22@gmail.com>
2020-12-06 21:52:01 +0530

    Final Code

--- lib/screens/manage_cards.dart
+++ lib/screens/manage_cards.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_credit_card/credit_card_model.dart';
-import 'package:mastering_payments/models/cards.dart';
+//import 'package:flutter_credit_card/credit_card_model.dart';
+//import 'package:mastering_payments/models/cards.dart';
 import 'package:mastering_payments/provider/user_provider.dart';
 import 'package:mastering_payments/screens/credit_card.dart';
 import 'package:mastering_payments/services/styles.dart';
 import 'package:provider/provider.dart';
+import 'package:mastering_payments/services/stripe.dart';
+import 'package:stripe_payment/stripe_payment.dart' as stripeKar;
 
 class ManagaCardsScreen extends StatefulWidget {
   @override
@@ -12,52 +14,77 @@ class ManagaCardsScreen extends StatefulWidget {
 }
 
 class _ManagaCardsScreenState extends State<ManagaCardsScreen> {
-
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context);
+    final StripeServices stripeServices = StripeServices();
+    // void payViaCard(String cardId, int amount) async {
+    //   try {
+
+    //     StripeServices stripeServices = StripeServices();
+    //     //String card = user.userModel.activeCard;
+    //     String stripeID = user.userModel.stripeId;
+    //     await stripeServices.payViaExistingCard(
+    //         amount: amount.toString(), currency: "inr",);
+    //     print("Kaam ZAla re !");
+    //   } catch (err) {
+    //     print("Baag ky zala :$err.toString()");
+    //   }
+    // }
 
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.white,
-        iconTheme: IconThemeData(color: Colors.green),
-        title: Text(
-          "Cards",
-          style: TextStyle(color: Colors.green),
-        ),
-      ),
-      body: ListView.builder(
-          itemCount: user.cards.length,
-          itemBuilder: (_, index){
-        return  Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            decoration:  BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: white,
-                boxShadow: [
-                  BoxShadow(
-                      color: grey[200],
-                      offset: Offset(2, 1),
-                      blurRadius: 5
-                  )
-                ]
-            ),
-            child: ListTile(
-              leading: Icon(Icons.credit_card),
-              title: Text("**** **** **** ${user.cards[index].last4}"),
-              subtitle: Text("${user.cards[index].month} / ${user.cards[index].year}"),
-              trailing: Icon(Icons.more_horiz),
-              onTap: (){
-
-              },
-            ),
+        appBar: AppBar(
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.white,
+          iconTheme: IconThemeData(color: Colors.green),
+          title: Text(
+            "Cards",
+            style: TextStyle(color: Colors.green),
           ),
-        );
-
-      })
-    );
+        ),
+        body: ListView.builder(
+            itemCount: user.cards.length,
+            itemBuilder: (_, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: white,
+                      boxShadow: [
+                        BoxShadow(
+                            color: grey[200],
+                            offset: Offset(2, 1),
+                            blurRadius: 5)
+                      ]),
+                  child: ListTile(
+                      leading: Icon(Icons.credit_card),
+                      title: Text("**** **** **** ${user.cards[index].last4}"),
+                      subtitle: Text(
+                          "${user.cards[index].month} / ${user.cards[index].year}"),
+                      trailing: Icon(Icons.more_horiz),
+                      onTap: () async {
+                        stripeKar.CreditCard currentCard = stripeKar.CreditCard(
+                          number: "424242424242",
+                          expMonth: user.cards[index].month,
+                          expYear: user.cards[index].year,
+                        );
+                        await stripeServices.getData();
+                        await stripeServices.payViaExistingCard(
+                            amount: 4500.toString(),
+                            currency: "inr",
+                            currentCard: currentCard);
+                        // try {
+                        //   await stripeServices.charge(amount: 2000);
+                        //   // customer: user.userModel.stripeId,
+                        //   // cardId: user.userModel.activeCard);
+                        // } catch (err) {
+                        //   print(err.toString());
+                        // }
+                      }),
+                ),
+              );
+            }));
   }
 }
--- lib/services/stripe.dart
+++ lib/services/stripe.dart
@@ -1,38 +1,115 @@
 import 'dart:convert';
 import 'package:dio/dio.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:stripe_payment/stripe_payment.dart' as stripkar;
 import 'package:mastering_payments/services/cards.dart';
-import 'package:mastering_payments/services/purchases.dart';
 import 'package:mastering_payments/services/user.dart';
-import 'package:uuid/uuid.dart';
+import 'package:mastering_payments/provider/user_provider.dart';
 
+class StripeTransactionResponse {
+  String message;
+  bool success;
+  StripeTransactionResponse({this.message, this.success});
+}
 
-class StripeServices{
-  static const PUBLISHABLE_KEY = "pk_test_A4nXLMSuUFOORBC9PE1x0TmK00Flkotazb";
-  static const SECRET_KEY = "sk_test_lkszgvX0S87KLLs7NmunPr5Z00F8lcHe6s";
+class StripeServices {
+  static String apiBase = 'https://api.stripe.com/v1';
+  static String paymentApiUrl = '${StripeServices.apiBase}/payment_intents';
+
+  static const PUBLISHABLE_KEY =
+      "pk_test_51HuzGEELsLSfcXBkZzSJLqyBPCMWtxqb9MFgAayLTOvDweihjZEYSXndRm6EY5rU3HtckouhI7EW3z7LkBsVjQw000VZqLhgh3";
+  static const SECRET_KEY =
+      "sk_test_51HuzGEELsLSfcXBkw1d7XKchJMTdsNZlWh6jeD7ANBRYo6Oq5OH3aVTr1FUap0Qq9mY332P8K8Ako3ObOtFR65NG00VLqcUjEm";
   static const PAYMENT_METHOD_URL = "https://api.stripe.com/v1/payment_methods";
   static const CUSTOMERS_URL = "https://api.stripe.com/v1/customers";
   static const CHARGE_URL = "https://api.stripe.com/v1/charges";
-  Map<String, String> headers = {
+
+  static Map<String, String> headers = {
     'Authorization': "Bearer  $SECRET_KEY",
     "Content-Type": "application/x-www-form-urlencoded"
   };
 
-  Future<String> createStripeCustomer({String email, String userId})async{
+  StripeServices() {
+    //
+  }
+
+  static getPlatformExceptionErrorResult(err) {
+    String message = 'Something went wrong';
+    if (err.code == 'cancelled') {
+      message = 'Transaction cancelled';
+    }
+
+    return new StripeTransactionResponse(message: message, success: false);
+  }
+
+  static Future<Map<String, dynamic>> createPaymentIntent(
+      String amount, String currency) async {
+    try {
+      Map<String, dynamic> body = {
+        'amount': amount,
+        'currency': currency,
+        'payment_method_types[]': 'card'
+      };
+      var response = await http.post(StripeServices.paymentApiUrl,
+          body: body, headers: StripeServices.headers);
+      return jsonDecode(response.body);
+    } catch (err) {
+      print('err charing user: ${err.toString()}');
+    }
+    return null;
+  }
+
+  Future<void> getData() async {
+    stripkar.StripeOptions(
+        publishableKey: StripeServices.PUBLISHABLE_KEY,
+        merchantId: "Test",
+        androidPayMode: 'test');
+  }
+
+  Future<StripeTransactionResponse> payViaExistingCard(
+      {String amount, String currency, stripkar.CreditCard currentCard}) async {
+    try {
+      // if (stripkar.StripePayment == null) print("Null aahe re baba");
+      // var paymentMethod = await stripkar.StripePayment.createPaymentMethod(
+      //     stripkar.PaymentMethodRequest(card: currentCard));
+
+      var paymentIntent =
+          await StripeServices.createPaymentIntent(amount, currency);
+      var response = await stripkar.StripePayment.confirmPaymentIntent(
+          stripkar.PaymentIntent(
+        clientSecret: paymentIntent['client_secret'],
+        paymentMethodId: "pm_1HvNuiELsLSfcXBkYFD6cZsU",
+      ));
+      if (response.status == 'succeeded') {
+        return new StripeTransactionResponse(
+            message: 'Transaction successful', success: true);
+      } else {
+        return new StripeTransactionResponse(
+            message: 'Transaction failed}', success: false);
+      }
+    } on PlatformException catch (err) {
+      return StripeServices.getPlatformExceptionErrorResult(err);
+    } catch (err) {
+      return new StripeTransactionResponse(
+          message: 'Transaction failed: ${err.toString()}', success: false);
+    }
+  }
+
+  Future<String> createStripeCustomer({String email, String userId}) async {
     Map<String, String> body = {
       'email': email,
     };
 
-    String stripeId = await http.post(CUSTOMERS_URL, body: body, headers: headers).then((response){
-     String stripeId = jsonDecode(response.body)["id"];
+    String stripeId = await http
+        .post(CUSTOMERS_URL, body: body, headers: headers)
+        .then((response) {
+      String stripeId = jsonDecode(response.body)["id"];
       print("The stripe id is: $stripeId");
       UserService userService = UserService();
-      userService.updateDetails({
-        "id": userId,
-        "stripeId": stripeId
-      });
+      userService.updateDetails({"id": userId, "stripeId": stripeId});
       return stripeId;
-    }).catchError((err){
+    }).catchError((err) {
       print("==== THERE WAS AN ERROR ====: ${err.toString()}");
       return null;
     });
@@ -40,57 +117,81 @@ class StripeServices{
     return stripeId;
   }
 
-  Future<void> addCard({int cardNumber, int month, int year, int cvc, String stripeId, String userId})async{
+  Future<void> addCard(
+      {int cardNumber,
+      int month,
+      int year,
+      int cvc,
+      String stripeId,
+      String userId}) async {
     Map body = {
       "type": "card",
       "card[number]": cardNumber,
       "card[exp_month]": month,
-      "card[exp_year]":year,
-      "card[cvc]":cvc
+      "card[exp_year]": year,
+      "card[cvc]": cvc
     };
-    Dio().post(PAYMENT_METHOD_URL, data: body, options: Options(contentType: Headers.formUrlEncodedContentType, headers: headers)).then((response){
+    Dio()
+        .post(PAYMENT_METHOD_URL,
+            data: body,
+            options: Options(
+                contentType: Headers.formUrlEncodedContentType,
+                headers: headers))
+        .then((response) {
       String paymentMethod = response.data["id"];
+
       print("=== The payment mathod id id ===: $paymentMethod");
-      http.post("https://api.stripe.com/v1/payment_methods/$paymentMethod/attach", body: {
-        "customer": stripeId
-      },
-      headers: headers
-      ).catchError((err){
+      http
+          .post(
+              "https://api.stripe.com/v1/payment_methods/$paymentMethod/attach",
+              body: {"customer": stripeId},
+              headers: headers)
+          .catchError((err) {
         print("ERROR ATTACHING CARD TO CUSTOMER");
         print("ERROR: ${err.toString()}");
-
       });
 
       CardServices cardServices = CardServices();
-      cardServices.createCard(id: paymentMethod, last4: int.parse(cardNumber.toString().substring(11)), exp_month: month, exp_year: year, userId: userId);
+      cardServices.createCard(
+          id: paymentMethod,
+          last4: int.parse(cardNumber.toString().substring(12)),
+          exp_month: month,
+          exp_year: year,
+          userId: userId);
       UserService userService = UserService();
-      userService.updateDetails({
-        "id": userId,
-        "activeCard": paymentMethod
-      });
+      userService.updateDetails({"id": userId, "activeCard": paymentMethod});
     });
   }
 
-  Future<bool> charge({String customer, int amount, String userId, String cardId, String productName})async{
-    Map<String, dynamic> data ={
+  Future<bool> charge({
+    String customer,
+    int amount,
+    String cardId,
+  }) async {
+    Map<String, dynamic> data = {
       "amount": amount,
-      "currency": "usd",
+      "currency": "inr",
       "source": cardId,
       "customer": customer
     };
-    try{
-      Dio().post(CHARGE_URL, data: data, options: Options(contentType: Headers.formUrlEncodedContentType, headers: headers)).then((response){
+    try {
+      Dio()
+          .post(CHARGE_URL,
+              data: data,
+              options: Options(
+                  contentType: Headers.formUrlEncodedContentType,
+                  headers: headers))
+          .then((response) {
         print(response.toString());
       });
-      PurchaseServices purchaseServices = PurchaseServices();
-      var uuid = Uuid();
-      var purchaseId = uuid.v1();
-      purchaseServices.createPurchase(id: purchaseId, amount: amount, cardId: cardId, userId: userId, productName: productName);
+      // PurchaseServices purchaseServices = PurchaseServices();
+      // var uuid = Uuid();
+      // var purchaseId = uuid.v1();
+      // purchaseServices.createPurchase(id: purchaseId, amount: amount, cardId: cardId, userId: userId, productName: productName);
       return true;
-    }catch(e){
+    } catch (e) {
       print("there was an error charging the customer: ${e.toString()}");
       return false;
     }
   }
-
 }
